611562b5715ab99861be512925eac6a80f019057
dgtyPedro <pedrogcs0104@gmail.com>
2021-08-09 09:24:19 -0300

    <refresh>

--- index.py
+++ index.py
@@ -12,7 +12,8 @@ import os
 from gtts import gTTS
 from googlesearch import search
 import webbrowser
-
+import time
+import pyautogui
 
 r=sr.Recognizer()
 with sr.Microphone() as source:                
@@ -35,9 +36,13 @@ if any(x in phrase_option for x in actionlist):
             textgo = phrase_list
             phrase_list.pop(0)  
 
-        def GoSearch(self):
-                       
+        def GoSearch(self):        
             what_search=''.join([str(item) for item in phrase_list])
+            speak="Searching {}".format(what_search)
+            output = gTTS(text=speak, lang='en', slow=False)  
+            output.save("output.mp3")
+            os.system("start output.mp3")
+            time.sleep(3)
             url = "https://www.google.com.tr/search?q={}".format(what_search)
             webbrowser.open_new_tab(url)
 
@@ -45,20 +50,38 @@ if any(x in phrase_option for x in actionlist):
             pass
 
         def GoBuy(self):
-            pass
-
+            what_buy=''.join([str(item) for item in phrase_list])
+            speak="Buying {}".format(what_buy)
+            output = gTTS(text=speak, lang='en', slow=False)  
+            output.save("output.mp3")
+            os.system("start output.mp3")
+            time.sleep(3)
+            url="www.google.com/search?hl=pt-BR&tbm=shop&sxsrf=ALeKk033V30FKlaPRoPs8IsqWmNmzEhyVg:1628253846073&q={}&tbas=0&tbs=vw:g&sa=X&ved=0ahUKEwj0w-SgtpzyAhUalZUCHTClCRkQvSsI1gQoAA&biw=1496&bih=723".format(what_buy)
+            webbrowser.open_new_tab(url)
+        
         def GoFind(self):
-            pass
+            what_find=''.join([str(item) for item in phrase_list])
+            speak="Finding {}".format(what_find)
+            output = gTTS(text=speak, lang='en', slow=False)  
+            output.save("output.mp3")
+            os.system("start output.mp3")
+            time.sleep(3)
+            url="www.google.com.tr/maps/search/{}/".format(what_find)
+            webbrowser.open_new_tab(url)
 
         def GoOpen(self):
-            pass
-    
-        
-    
-    
+            what_open=''.join([str(item) for item in phrase_list])
+            speak="Opening {}".format(what_open)
+            output = gTTS(text=speak, lang='en', slow=False)  
+            output.save("output.mp3")
+            os.system("start output.mp3")
+            time.sleep(3)
+            pyautogui.press('Win')
+            time.sleep(1)
+            pyautogui.write('{}'.format(what_open), interval=0.1)
+            pyautogui.press('Enter')
+            
 
-
-    
     run=IaSearch()
     if phrase_option=='search':  
         run.GoSearch()
